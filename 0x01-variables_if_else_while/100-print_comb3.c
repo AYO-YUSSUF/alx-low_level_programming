5690eba92c11653d4dc987257154245e5684597d
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-11 18:48:55 -0700

    conditional statements 2.3 updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,8 +17,8 @@ int main(void)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
-				putchar(num + 48);
-				putchar(num + 48);
+				putchar(num1 + 48);
+				putchar(num2 + 48);
 				if (num1 + num2 != 17)
 				{
 					putchar(',');
