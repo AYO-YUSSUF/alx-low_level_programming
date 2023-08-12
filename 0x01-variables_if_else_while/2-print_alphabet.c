8b3f5cd52bb4784b553a6d166a11aaca07494311
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-11 17:15:50 -0700

    conditional statements 0.7

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print the Alphabets A-Z
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph += 'z')
+	{
+		putchar(alph);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
