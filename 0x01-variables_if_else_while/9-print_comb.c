26098b6f856942d2131c6f6d8f5ffa05a477b47f
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-11 18:30:50 -0700

    conditional statements 1.9 updated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print - 9, 0, 1
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + 48);
+
+		if (num != 9)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
