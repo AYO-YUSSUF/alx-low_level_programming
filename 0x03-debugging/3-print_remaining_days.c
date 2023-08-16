3d7ab7f82a17f38d9ee29b5aa9ae8030ed8e41ac
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-16 11:15:45 -0700

    debugging commit 4.3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,10 +5,14 @@
  * print_remaining_days - print the remaining days of the year
  * @month: month in number format
  * @day: day of the month
- * @year: the year
+ * @year: year
 */
 void print_remaining_days(int month, int day, int year)
 {
+	/**
+	 * leap year when year can be divided
+	 * by 100 and 400 or by 4 evenly
+	*/
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
@@ -16,7 +20,7 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
-		printf("remaining days: %d\n", 366 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
