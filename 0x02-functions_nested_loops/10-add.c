e184590d870cec572fbabcb640ad148969cae014
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-15 17:02:13 -0700

    nested loop commits 1.1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two digits
+ *
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: sum of input a & b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	
+	return (sum);
+}
