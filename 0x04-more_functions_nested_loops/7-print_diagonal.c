f4d4331dd6fd7da4eda7d34fd5ea30de85b40dac
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-17 12:35:43 -0700

    more loops commit 1.0

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,17 @@
 
 void print_diagonal(int n)
 {
-	int diag, space;
+	int postn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (diag = 1; diag <= n; diag++)
+		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= diag; space++)
+			for (space = 1; space <= postn; space++)
 				_putchar(' ');
-			_putchar(92); /* equal to `/` in ASCII*/
+			_putchar(92); /* equal to '/' in ASCII*/
 			_putchar('\n');
 		}
 	}
