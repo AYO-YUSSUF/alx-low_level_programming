701f48fded71dcbb7a05e442ee37966606110c9c
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-17 12:14:37 -0700

    more loops commit 0.9

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ * @n: input is the number of times `\` will be printed
+*/
+
+void print_diagonal(int n)
+{
+	int diag, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (diag = 1; lnchar <= n; diag++)
+		{
+			for (space = 1; space <= diag; space++)
+				_putchar('_');
+			_putchar(92); /* equal to `/` in ASCII*/
+			_putchar('\n');
+		}
+	}
+}
