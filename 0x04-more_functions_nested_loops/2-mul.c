144f52a7075adb06eb0b8213862862f05a0e90e3
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-17 09:19:07 -0700

    more loops commit 0.3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply two digits
+ *
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: mult of input a & b
+*/
+
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+}
