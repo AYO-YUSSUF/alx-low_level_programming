7af6a066116e7f796a852a77bbee177a1f16fa29
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-21 17:47:08 -0700

    pointers and arrays task 9 update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,14 @@
  * Return: 'dest' pointer
 */
 
-void *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
 	do {
 		i++;
 		dest[i] = src[i];
-	} while (src[i] != '\0')
+	} while (src[i] != '\0');
 
 	return (dest);
 }
