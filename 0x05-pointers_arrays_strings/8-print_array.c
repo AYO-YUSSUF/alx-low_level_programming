55a83631080fb615f26476215111a8c73c34d8c3
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-21 18:29:00 -0700

    pointers and arrays task 8 update 1

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < *a; i++)
 	{
 		if (i != (n - 1))
 			printf("%d, ", a[i]);
 		else
-			break
+			printf("%d, ", a[i]);
 	}
 	printf("\n");
 }
