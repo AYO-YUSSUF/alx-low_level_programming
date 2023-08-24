157993c5a5e62f57542eff31f64150c1a885d507
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-24 08:40:58 -0700

    first 5 task of 06 pointer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate string and integter
+ * @dest: destination pointer input
+ * @src: source pointer input
+ * @n: most number of byte from src
+ * 
+ * Return: string @dest 
+*/
+
+char *_strcat(char *dest, char *src, int n)
+{
+    int a, i;
+    a = 0;
+    /*find the dest array*/
+    while (dest[a])
+            a++;
+    /* src wont be null terminated if it contain n or more bytes*/
+    for (i = 0; i < n && src[i] != '\0'; i++)
+           dest[a + i] = src[i];
+      /* null terminate dest*/
+    return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an integer array 
+ * @a: an integter array 
+ * @n: number of elements 
+ * 
+ * Return: anything 
+*/
+
+void reverse_array(int *a, int n)
+{
+    int x, y, z;
+    
+    for (x = 0, y = (n - 1); x < y; x++, y--)
+    {
+        z = a[x];
+        a[x] = a[y];
+        a[y] = z;
+        
+    }
+}
