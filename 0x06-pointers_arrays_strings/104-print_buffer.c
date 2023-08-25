7595bb878e190d6339dcd0615237a5587f8d5367
AYO YUSSUF <yussufquayyum@gmail.com>
2023-08-24 19:32:38 -0700

    1-12 0x6 pointers commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,11 +9,11 @@
 
 int isPrintableASCII(int n)
 {
-    return (n >= 32 && c <= 126);
+	return (n >= 32 && c <= 126);
 }
 
 /**
- * printHexes - prints hex values for string b 
+ * printHexes - prints hex values for string b
  * @b: string to print
  * @start: starting
  * @end: ending
@@ -21,20 +21,18 @@ int isPrintableASCII(int n)
 
 int printHexes(char *b, int start, int end)
 {
-    int i = 0;
-    
-    while (i < 10)
-    {
-        if (i < end)
-            printf("%02x", *(b + start + i));
-        else 
-            printf("");
-        if (i % 2)
-            printf("");
-        i++;
-        
-    }
-    
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("");
+		if (i % 2)
+			printf("");
+		i++;
+	}
 }
 
 /**
@@ -46,46 +44,40 @@ int printHexes(char *b, int start, int end)
 
 void printASCII(char *b, int start, int end)
 {
-    int as, i = 0;
-    
-    while (i < end)
-    {
-        as = *(b + i + start);
-        if (!isPrintableASCII(as))
-            as = 46;
-        printf("%c", as);
-        i++;
-        
-    }
-    
+	int as, i = 0;
+
+	while (i < end)
+	{
+		as = *(b + i + start);
+		if (!isPrintableASCII(as))
+			as = 46;
+		printf("%c", as);
+		i++;
+	}
 }
 
 
 /**
- * print_buffer - print a buffer 
+ * print_buffer - print a buffer
  * @b: string to print
  * @size: buffer size
 */
 
 void print_buffer(char *b, int size)
 {
-    int start, end;
-    
-    if (size > 0)
-    {
-        for (start = 0; start < size; start += 10)
-        {
-            end = (size - start < 10) ? size - start : 10;
-            printf("%08x:", start);
-            printHexes(b, start, end);
-            printASCII(b, start, end);
-            printf("\n");
-            
-        }
-        
-    }
-    else 
-        printf("\n");
-    
-}
+	int start, end;
 
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x:", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
+}
