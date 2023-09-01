b083ac63bc42c4bddc792aaca493bb0b4c9af80c
AYO YUSSUF <yussufquayyum@gmail.com>
2023-09-01 15:22:20 -0700

    argc task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number and new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
