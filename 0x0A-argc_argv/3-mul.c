8e09ed754cb8e1f00e3ad36ab98a4051aa0f8c2a
AYO YUSSUF <yussufquayyum@gmail.com>
2023-09-01 15:35:31 -0700

    argc task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs multiplication
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	int a, s;
+
+	if (argc < 3)
+	{
+		printf("argc error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	s = atoi(argv[2]);
+	printf("%d\n", a * s);
+
+	return (0);
+}
