772248233438b0dad674b8cb25e3bcfd3530004d
AYO YUSSUF <yussufquayyum@gmail.com>
2023-09-07 15:37:40 -0700

    more malloc task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 
 /**
- * *_memset - fills memory
- * @s: pointer
- * @b: constant
- * @n: bytes max
- * Return: s
-*/
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *pt = s;
-
-	while (n--)
-		*s++ = b;
-
-	return (pt);
-}
-
-/**
  * *_calloc - allocate memory for array
  * @nmemb: array len
  * @size: element size
@@ -28,18 +10,15 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *x;
+	char *x;
+	unsigned int i;
 
-	if (size == 0 || nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	x = malloc(sizeof(int) * nmemb);
-
-	if (x == 0)
-	{
+	x = malloc(nmemb * size);
+	if (x == NULL)
 		return (NULL);
-	}
-
-	_memset(x, 0, sizeof(int) * nmemb);
-
+	for (i = 0; i < (nmemb * size); i++)
+		x[i] = 0;
 	return (x);
 }
