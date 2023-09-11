055562d5c6947c6053e1e31029b0be254d59ce5b
AYO YUSSUF <yussufquayyum@gmail.com>
2023-09-11 15:59:44 -0700

    struct typedef task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog struct
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Description: lonely dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
